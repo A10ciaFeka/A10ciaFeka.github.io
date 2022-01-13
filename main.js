aebd481774d460aa2019d1cb22076e75db583a32
A10ciaFeka <jag0003@alu.medac.es>
2022-01-13 18:58:56 +0100

    movimiento villano y examenes

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ window.onload = ()=>{
         return Math.floor(Math.random()*7);
     }
     
-
+    const protagonista = 'O';
+    const salida = 'S';
+    const examenes = 'Y';
+    const villano = 'X';
 
     let tabla = document.querySelectorAll('tr');
 
@@ -23,8 +26,8 @@ window.onload = ()=>{
             }    
         }
 
-        tabla[0].children[0].textContent = 'O';
-        tabla[7].children[7].textContent = 'S';
+        tabla[0].children[0].textContent = protagonista;
+        tabla[7].children[7].textContent = salida;
 
         let prep = 0;
 
@@ -34,10 +37,10 @@ window.onload = ()=>{
             
             if(!((ale1==0 && ale2==0)||(ale1==7&&ale2==7))){
                 if(prep==0){
-                    tabla[ale1].children[ale2].textContent = 'X';
+                    tabla[ale1].children[ale2].textContent = villano;
                     prep++;
-                }else if(prep==1 && tabla[ale1].children[ale2].textContent!='X'){
-                    tabla[ale1].children[ale2].textContent = 'Y';
+                }else if(prep==1 && tabla[ale1].children[ale2].textContent!=villano){
+                    tabla[ale1].children[ale2].textContent = examenes;
                     prep++;
                 }
                 
@@ -54,10 +57,10 @@ window.onload = ()=>{
     *
     * 
     */
-    function posProtagonista(){
+    function posElemento(cadena){
         for(let fila = 0; fila<tabla.length; fila++){
             for(let columna = 0; columna<tabla.length; columna++){
-                if(tabla[fila].children[columna].textContent == 'O'){
+                if(tabla[fila].children[columna].textContent == cadena){
                     return new Array(fila,columna); 
                 }
             } 
@@ -67,21 +70,69 @@ window.onload = ()=>{
     /*
     *
     *
+    * Movimiento del malo
+    *
+    * 
+    */
+    
+    function movimientoVillano(){
+        let filaFinal;
+        if(posElemento(protagonista)[0]>posElemento(villano)[0]){
+            filaFinal = posElemento(protagonista)[0] - posElemento(villano)[0];
+        }else{
+            filaFinal = posElemento(villano)[0] - posElemento(protagonista)[0];
+        }
+
+        let columnaFinal;
+        if(posElemento(protagonista)[1]>posElemento(villano)[1]){
+            columnaFinal = posElemento(protagonista)[1] - posElemento(villano)[1];
+        }else{
+            columnaFinal = posElemento(villano)[1] - posElemento(protagonista)[1];
+        }
+
+        console.log(new Array(filaFinal,columnaFinal));
+        console.log(posElemento(protagonista) + ' protagonista');
+        console.log(posElemento(villano) + ' villano');
+        
+        tabla[posElemento(villano)[0]].children[posElemento(villano)[1]].textContent = '.';
+        if(tabla[filaFinal].children[columnaFinal].textContent!=examenes){
+            tabla[filaFinal].children[columnaFinal].textContent = villano;
+        }
+
+    }
+
+    /*
+    *
+    *
     * Le añado las propiedades onclick a los botones
     *
     * 
     */
+   
+    function cambiarColor(){
+        document.querySelector('.examen').style.backgroundColor='green';
+    }
+
     let buttons = Array.from(document.querySelector('.botones').children);
 
     buttons.forEach((item,index) => {
         switch(index){
             case 0: //Arriba 
                 
-                item.onclick = ()=>{
-                    let posAntigua = posProtagonista();
+                item.onclick = ()=>{ 
+                    let posAntigua = posElemento(protagonista);
+
                     if(posAntigua[0]!=0){
-                        tabla[posProtagonista()[0]-1].children[posProtagonista()[1]].textContent = 'O';
+                        if(document.querySelector('.examen').style.backgroundColor!='green'){
+                            if( ((posElemento(protagonista)[0]-1) == posElemento(examenes)[0]) && (posElemento(protagonista)[1] == posElemento(examenes)[1]) )
+                            {
+                                cambiarColor();
+                            }
+                            
+                        }
+                        tabla[posElemento(protagonista)[0]-1].children[posElemento(protagonista)[1]].textContent = protagonista;
                         tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
+                        movimientoVillano();
                     }
                 }
 
@@ -90,10 +141,17 @@ window.onload = ()=>{
             case 1: //Izquierda
                 
                 item.onclick = ()=>{
-                    let posAntigua = posProtagonista();
+                    let posAntigua = posElemento(protagonista);
                     if(posAntigua[1]!=0){
-                        tabla[posProtagonista()[0]].children[posProtagonista()[1]-1].textContent = 'O';
+                        if(document.querySelector('.examen').style.backgroundColor!='green'){
+                            if( (posElemento(protagonista)[0] == posElemento(examenes)[0]) && ((posElemento(protagonista)[1]-1) == posElemento(examenes)[1]) )
+                            {
+                                cambiarColor();
+                            }
+                        }
+                        tabla[posElemento(protagonista)[0]].children[posElemento(protagonista)[1]-1].textContent = protagonista;
                         tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
+                        movimientoVillano();
                     }   
                 }
 
@@ -102,10 +160,26 @@ window.onload = ()=>{
             case 2: //Derecha
 
                 item.onclick = ()=>{
-                    let posAntigua = posProtagonista();
+                    let posAntigua = posElemento(protagonista);
                     if(posAntigua[1]!=tabla.length-1){
-                        tabla[posProtagonista()[0]].children[posProtagonista()[1]+1].textContent = 'O';
+                        if(document.querySelector('.examen').style.backgroundColor!='green'){
+                            if( (posElemento(protagonista)[0] == posElemento(examenes)[0]) && ((posElemento(protagonista)[1]+1) == posElemento(examenes)[1]) )
+                            {
+                                cambiarColor();
+                            }                        
+                            tabla[posElemento(protagonista)[0]].children[posElemento(protagonista)[1]+1].textContent = protagonista;
+                        }else{
+                            if(tabla[posElemento(protagonista)[0]].children[posElemento(protagonista)[1]+1].textContent == salida){
+                                
+                                tabla[posElemento(protagonista)[0]].children[posElemento(protagonista)[1]+1].textContent = protagonista;
+                                tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
+                                alert('¡Has ganado!');
+                            }else{
+                                tabla[posElemento(protagonista)[0]].children[posElemento(protagonista)[1]+1].textContent = protagonista; 
+                            }
+                        }
                         tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
+                        movimientoVillano();
                     }      
                 }
 
@@ -114,18 +188,34 @@ window.onload = ()=>{
             case 3: //Abajo
 
                 item.onclick = ()=>{
-                    let posAntigua = posProtagonista();
+                    let posAntigua = posElemento(protagonista);
                     if(posAntigua[0]!=tabla.length-1){
-                        tabla[posProtagonista()[0]+1].children[posProtagonista()[1]].textContent = 'O';
+
+                        if(document.querySelector('.examen').style.backgroundColor!='green'){
+
+                            if( ((posElemento(protagonista)[0]+1) == posElemento(examenes)[0]) && (posElemento(protagonista)[1] == posElemento(examenes)[1]) )
+                            {
+                                cambiarColor();
+                            }                        
+                            tabla[posElemento(protagonista)[0]+1].children[posElemento(protagonista)[1]].textContent = protagonista;
+                        }else{
+                            if(tabla[posElemento(protagonista)[0]+1].children[posElemento(protagonista)[1]].textContent == salida){
+                                
+                                tabla[posElemento(protagonista)[0]+1].children[posElemento(protagonista)[1]].textContent = protagonista;
+                                tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
+                                alert('¡Has ganado!');
+                            }else{
+                                tabla[posElemento(protagonista)[0]+1].children[posElemento(protagonista)[1]].textContent = protagonista;
+                            }
+                        }
                         tabla[posAntigua[0]].children[posAntigua[1]].textContent = '.';
-                        
+                        movimientoVillano();
                     }    
+                    
                 }
 
             break;
-    
         }
-        
     });
 
 
