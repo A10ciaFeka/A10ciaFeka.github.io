3846df196f7c344b4e7301454e963606067a14d8
A10ciaFeka <jag0003@alu.medac.es>
2022-02-04 09:10:57 +0100

    mk

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "localhost",
-    database: "puntuacion",
-    user: "root",
-    password: ""
+    host: "eu-cdbr-west-02.cleardb.net",
+    database: "heroku_7652922298a0c7a",
+    user: "b86c0fee349ce9",
+    password: "14353ad8"
 });
 
 con.connect((err)=>{
