3adbe577c51935563c8a282542afc50b51bbc90c
A10ciaFeka <jag0003@alu.medac.es>
2022-02-04 16:19:19 +0100

    probemos

---
+++ js/historia.js
@@ -0,0 +1,45 @@
+window.onload = ()=>{
+            
+    document.querySelector('h2').style.visibility = 'visible';
+    document.querySelector('h2').style.animationName = 'efectoBlur';
+    document.querySelector('h2').style.animationDuration = '5s';
+
+    let par = document.querySelectorAll('p');
+    let contador = 0;
+
+    const timer = setInterval(()=>{
+        if(contador==par.length){
+            clearInterval(timer);
+        }else{
+            par[contador].style.visibility = 'visible';
+            par[contador].style.animationName = 'efectoBlur';
+            par[contador].style.animationDuration = '5s';
+            contador++;
+        }
+    },4500)
+
+    document.querySelector('#menu').addEventListener('click',(e)=>{
+        let sonido = document.createElement('audio');
+        sonido.src = 'assets/btpress.mp3';
+        sonido.autoplay = true;
+        sonido.hidden = true;
+
+        document.body.appendChild(sonido);
+
+        window.setTimeout(function(){
+            document.body.removeChild(sonido);
+            window.location.href = 'index.html';
+        },1800);
+    })
+
+    document.querySelector('#mute').addEventListener('click',(e)=>{
+        let cancion = document.querySelector('#cancion');
+        if(cancion.muted){
+            cancion.muted = false;
+            document.querySelector('#mute').firstChild.textContent = 'volume_up';
+        }else{
+            cancion.muted = true;
+            document.querySelector('#mute').firstChild.textContent = 'volume_off';
+        }
+    });
+}
